fdc8f5aa4635f5c7b59194e29dc5660d625d1bbb
Graham Turner <turnertgraham@gmail.com>
2019-02-23 20:33:11 -0500

    initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,57 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { css } from "@emotion/core"
+import { StaticQuery, Link, graphql } from "gatsby"
+
+import { rhythm } from "../utils/typography"
+
+export default ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+}
+    render={data => (
+      <div
+        css={css`
+          margin: 0 auto;
+          max-width: 700px;
+          padding: ${rhythm(2)};
+          padding-top: ${rhythm(1.5)};
+        `}
+      >
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Graham's Site</title>
+          <link rel="canonical" href="https://tgturner.com" />
+        </Helmet>
+        <Link to={`/`}>
+          <h3
+            css={css`
+              margin-bottom: ${rhythm(2)};
+              display: inline-block;
+              font-style: normal;
+            `}
+          >
+            {data.site.siteMetadata.title}
+          </h3>
+        </Link>
+        <Link
+          to={`/about/`}
+          css={css`
+            float: right;
+          `}
+        >
+          About
+        </Link>
+        {children}
+      </div>
+    )}
+  />
+)
